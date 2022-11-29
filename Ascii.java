0be0fb610460d2423457a019ae66d93dc4b248fa
varunsai8074 <118423177+varunsai8074@users.noreply.github.com>
2022-11-29 17:20:19 +0530

    Create Ascii.java
    ASCII - American Standard Code for Information Interchange. Write a program to get a character as input and print it's ASCII value. When a character is entered, the character itself  stored.  And when we displayed that value using "int()"  type casting , the entered character is displayed as Ascii value. NOTE: Bold letters or words are Input and rest are output statements.
    
    Sample Input :
    
    g
    
    Sample Output:
    
    103

---
+++ Ascii.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Ascii
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        char test = sc.next().charAt(0);
+        int test1 = test;
+        System.out.println(test1);
+        
+    }
+}
