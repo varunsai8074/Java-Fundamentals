1ed529239311fd1b8a18db24dacf4964ad63d845
varunsai8074 <118423177+varunsai8074@users.noreply.github.com>
2022-11-29 17:08:16 +0530

    Create InputOutput2.java
    Write a program to get float as input and print it. When the user enters a float, it is stored in the variable test Float. Use '%.2f' in the print() to display float value with 2 precisions. NOTE: Bold letters or words are Input and the rest are output statements.
    
    Sample Input :
    
    56.2425
    
    Sample Output :
    
    56.24

---
+++ InputOutput2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class InputOutput2
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        float test = sc.nextFloat();
+        System.out.printf("%.2f",test);
+    }
+}
