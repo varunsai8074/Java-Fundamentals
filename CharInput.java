7777bba97892ed6eb18acaea4e2b1f962643821b
varunsai8074 <118423177+varunsai8074@users.noreply.github.com>
2022-11-29 17:10:23 +0530

    Create CharInput.java
    Write a program to get a character as input and print it. When the user enters a character, it is stored in the variable test Character. NOTE: Bold letters or words are Input and the rest are output statements.
    
    Sample Input:
    
    z
    
    Sample Output:
    
    z

---
+++ CharInput.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CharInput
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        char test = sc.next().charAt(0);
+        System.out.println(test);
+    }
+}
