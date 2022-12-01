49c7913120a9599d7690272ff93d6e62edb82e9a
varunsai8074 <118423177+varunsai8074@users.noreply.github.com>
2022-12-01 10:26:28 +0530

    Create Harshard.java

---
+++ Harshard.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Harshard
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        int n = sc.nextInt();
+        int sum =0,x;
+        x=n;
+        while(n > 0)
+        {
+            int rem = n % 10;
+            n = n/10;
+            sum = sum + rem;
+        }
+        if(x % sum == 0)
+        {
+            System.out.println("Harshad Number");
+        }
+        else
+        {
+            System.out.println("Not Harshad Number");
+        }
+        
+    }
+}
